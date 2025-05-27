4775b2a5e6bab403cb909a1cc642e3f9253a39aa
Benjamin Leblanc <beni-leblanc@hotmail.com>
2025-05-27 13:11:26 -0400

    [PATCH] Correctly create the columns for the result analysis concerning the Disintegrated bounds.

--- graphics/wandb_results_analysis.py
+++ graphics/wandb_results_analysis.py
@@ -70,40 +70,39 @@ def generate_latex_table(results):
         last_dataset = results[i][0]
         if results[i][1] == 'FO':
             FO = np.round(results[i][3] * 100, 1)
-            FO_std = np.round(results[i][7] * 100, 1)
-            FO_err = np.round(results[i][4] * 100, 1)
-            FO_err_std = np.round(results[i][8] * 100, 1)
+            FO_std = np.round(results[i][11] * 100, 1)
+            FO_err = np.round(results[i][6] * 100, 1)
+            FO_err_std = np.round(results[i][14] * 100, 1)
             Ben = np.round(results[i][2] * 100, 1)
-            Ben_std = np.round(results[i][6] * 100, 1)
-            Ben_err = np.round(results[i][4] * 100, 1)
-            Ben_err_std = np.round(results[i][8] * 100, 1)
+            Ben_std = np.round(results[i][10] * 100, 1)
+            Ben_err = np.round(results[i][7] * 100, 1)
+            Ben_err_std = np.round(results[i][15] * 100, 1)
+            Dis_KL = np.round(results[i][4] * 100, 1)
+            Dis_KL_std = np.round(results[i][5] * 100, 1)
+            Dis_KL_err = np.round(results[i][8] * 100, 1)
+            Dis_KL_err_std = np.round(results[i][9] * 100, 1)
         elif results[i][1] == 'SO':
             SO = np.round(results[i][3] * 100, 1)
-            SO_std = np.round(results[i][7] * 100, 1)
-            SO_err = np.round(results[i][4] * 100, 1)
-            SO_err_std = np.round(results[i][8] * 100, 1)
+            SO_std = np.round(results[i][11] * 100, 1)
+            SO_err = np.round(results[i][6] * 100, 1)
+            SO_err_std = np.round(results[i][14] * 100, 1)
         elif results[i][1] == 'Bin':
             Bin = np.round(results[i][3] * 100, 1)
-            Bin_std = np.round(results[i][7] * 100, 1)
-            Bin_err = np.round(results[i][4] * 100, 1)
-            Bin_err_std = np.round(results[i][8] * 100, 1)
+            Bin_std = np.round(results[i][11] * 100, 1)
+            Bin_err = np.round(results[i][6] * 100, 1)
+            Bin_err_std = np.round(results[i][14] * 100, 1)
         elif results[i][1] == 'Dis_Renyi':
-            Dis_Renyi = np.round(results[i][3] * 100, 1)
-            Dis_Renyi_std = np.round(results[i][7] * 100, 1)
-            Dis_Renyi_err = np.round(results[i][4] * 100, 1)
-            Dis_Renyi_err_std = np.round(results[i][8] * 100, 1)
-        elif results[i][1] == 'Dis_KL':
-            Dis_KL = np.round(results[i][3] * 100, 1)
-            Dis_KL_std = np.round(results[i][7] * 100, 1)
-            Dis_KL_err = np.round(results[i][4] * 100, 1)
-            Dis_KL_err_std = np.round(results[i][8] * 100, 1)
+            Dis_Renyi = np.round(results[i][4] * 100, 1)
+            Dis_Renyi_std = np.round(results[i][5] * 100, 1)
+            Dis_Renyi_err = np.round(results[i][8] * 100, 1)
+            Dis_Renyi_err_std = np.round(results[i][9] * 100, 1)
     print(f"{last_dataset} & {FO} $\pm$ {FO_std} & {FO_err} $\pm$ {FO_err_std} & {SO} $\pm$ {SO_std} & {SO_err} $\pm$ {SO_err_std} & {Bin} $\pm$ {Bin_std} & {Bin_err} $\pm$ {Bin_err_std} & {Dis_Renyi} $\pm$ {Dis_Renyi_std} & {Dis_Renyi_err} $\pm$ {Dis_Renyi_err_std} & {Dis_KL} $\pm$ {Dis_KL_std} & {Dis_KL_err} $\pm$ {Dis_KL_err_std} & {Ben} $\pm$ {Ben_std} & {Ben_err} $\pm$ {Ben_err_std} \\\ ")
 
-file = 'wandb_export_2025-05-22T09_49_29.224-04_00.csv'
+file = 'wandb_export_2025-05-23T10_47_43.825-04_00.csv'
 hyperparams = ['M', 'batch_size', 'bootstrap', 'dataset', 'delta', 'distribution', 'is_using_wandb', 'lr',
                'num_epochs', 'num_trials', 'num_workers', 'opt_bound', 'pred', 'prior', 'project_name', 'rand_n',
-               'risk', 'seed', 'stochastic', 'stump_init', 'tree_depth', 'type', 'uniform']
-num_results = ['ben_bound_with_finetune', 'deterministic_bound', 'test-error_finetune', 'test-error_std_finetune']  # Do not change that order
+               'risk', 'stochastic', 'stump_init', 'type', 'uniform']
+num_results = ['ben_bound_with_finetune', 'deterministic_bound', 'deterministic_bound_sampled', 'deterministic_bound_sampled_std', 'test-error', 'test-error_finetune', 'test-error_sampled', 'test-error_sampled_std']  # Do not change that order
 final_distinction = ['dataset', 'risk']   # Do not change that order
 csv_to_latex(file_name=file,
              hyperparameters=hyperparams,
