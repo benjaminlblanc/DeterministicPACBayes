60f4c8f52a79c44cc597348a82797520a2ada874
beleb91 <beni-leblanc@hotmail.com>
2025-07-07 15:04:23 -0400

    Making the .sh files cleaner.

--- real_data_independent.sh
+++ real_data_independent.sh
@@ -5,42 +5,32 @@ m=10
 trials=5
 seed=541944
 
-for d in MUSH SVMGUIDE HABER TTT CODRNA ADULT PHIS
+for d in ADULT CODRNA MUSH HABER PHIS SVMGUIDE TTT
 do
-  for r in FO
-  do
-    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=dirichlet model.prior=1 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=ones training.seed=$seed
-    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=dirichlet model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=ones training.seed=$seed
-    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=gaussian model.prior=0 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.seed=$seed
-    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=categorical model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.seed=$seed
-	done
+  r=FO
+  python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.1 training.distribution=categorical model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.seed=$seed
+  python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.1 training.distribution=dirichlet model.prior=1 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=ones training.seed=$seed
+  python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.1 training.distribution=dirichlet model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=ones training.seed=$seed
+  python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.01 training.distribution=gaussian model.prior=0 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.seed=$seed
 
-	for r in SO
-  do
-    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=dirichlet model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=ones training.seed=$seed
-    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=gaussian model.prior=0 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.seed=$seed
-    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=categorical model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.seed=$seed
-	done
+	r=SO
+	python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.1 training.distribution=categorical model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.seed=$seed
+  python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.01 training.distribution=gaussian model.prior=0 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.seed=$seed
 
-	for r in Bin
+	r=Bin
+  for r_n in 10 100
   do
-    for r_n in 10 100
-    do
-      python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=dirichlet model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=ones training.rand_n=$r_n training.seed=$seed
-      python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=gaussian model.prior=0 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.rand_n=$r_n training.seed=$seed
-      python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=categorical model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.rand_n=$r_n training.seed=$seed
-    done
-	done
+    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.1 training.distribution=categorical model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.rand_n=$r_n training.seed=$seed
+    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.01 training.distribution=gaussian model.prior=0 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand training.rand_n=$r_n training.seed=$seed
+  done
 
-	for r in Dis_Renyi
+	r=Dis_Renyi
+  for ordr in 1.05 1.5 2 5 10
   do
-    for ordr in 1.05 1.2 1.5 1.8 1.95
-    do
-      python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=gaussian model.prior=0 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand bound.order=$ordr bound.n_grid=5 training.seed=$seed
-    done
-    for ordr in 1.05 1.5 2 5 10
-    do
-      python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.distribution=categorical model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand bound.order=$ordr bound.n_grid=5 training.seed=$seed
-    done
-	done
+    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.1 training.distribution=categorical model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand bound.order=$ordr bound.n_grid=5 training.seed=$seed
+  done
+  for ordr in 1.05 1.2 1.5 1.8 1.95
+  do
+    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.01 training.distribution=gaussian model.prior=0 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand bound.order=$ordr bound.n_grid=5 training.seed=$seed
+  done
 done
