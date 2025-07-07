Fixed the computation of the Bin baseline.

core/losses.py
@@ -1,29 +1,16 @@
 import torch
 
-from core.utils import BetaInc, Phi, multinomial_cdf_precomputations
+from core.utils import BetaInc, Phi, multinomial_cdf_precomputations, log_prob_bin
 
 
-def bin_loss(y_target, y_pred, theta, distribution, classif_type, n=100):
-
-    if distribution == 'dirichlet':
-        correct = torch.where(y_target == y_pred, theta, torch.zeros(1)).sum(1)
-        wrong = torch.where(y_target != y_pred, theta, torch.zeros(1)).sum(1)
-        w_theta = [BetaInc.apply(c, w, torch.tensor(0.5), torch.tensor(1)) for c, w in zip(correct, wrong)]
-
-    elif distribution == 'gaussian':
-        inner_Phi = (torch.squeeze(y_target) * torch.sum(torch.reshape(theta, (1, -1)) * y_pred, dim=1)) / torch.sum(y_pred ** 2, dim=1) ** 0.5
-        w_theta = Phi(inner_Phi)
-
-    elif distribution == 'categorical':
-        w_theta = torch.where(y_target != y_pred, theta, torch.tensor(0.)).sum(1)
-
-    return torch.stack([BetaInc.apply(torch.tensor(n // 2 + 1), torch.tensor(n // 2), w, torch.tensor(1)) for w in w_theta])
-
+def bin_loss(y_target, y_pred, theta, distribution, n_classes, n=100):
+    first_order_loss = moment_loss(y_target, y_pred, theta, distribution, n_classes, order=1)
+    bin_loss = torch.zeros(len(first_order_loss))
+    for i in range(n // 2, n + 1):
+        bin_loss += torch.exp(log_prob_bin(torch.tensor(i), torch.tensor(n), first_order_loss))
+    return bin_loss
 
 def moment_loss(y_target, y_pred, theta, distribution, n_classes, order=1):
-
-    assert order in [1, 2], "only first and second order supported atm"
-
     if distribution == 'dirichlet':
         correct = torch.where(y_target == y_pred, theta, torch.zeros(1)).sum(1)
         wrong = torch.where(y_target != y_pred, theta, torch.zeros(1)).sum(1)

core/utils.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import numpy as np
 import random
@@ -20,11 +21,8 @@ def whether_to_run_run(cfg):
         assert cfg.model.prior == 0
         if cfg.training.risk == "Dis_Renyi":
             assert 1 < cfg.bound.order < 2
-
     if cfg.training.risk == "Bin":
         assert cfg.training.rand_n > 0
-
-
 
 def updating_first_seed_results(seed_results, time, model, train_err, test_err, deterministic_bound, final_bound, ben_bound_no_finetune):
     seed_results["train-error"] = train_err['error']
@@ -54,6 +52,29 @@ def updating_last_seed_results(seed_results, cfg, train_error, test_error, ben_b
     else:
         seed_results["factor_with_finetune"] = 0
     return seed_results
+
+def log_stirling_approximation(n):
+    """
+    Stirling's approximation for the logarithm of the factorial
+    """
+    if n == 0:
+        return 0
+    return n * torch.log(n) - n + 0.5 * torch.log(2 * math.pi * n)
+
+
+def log_binomial_coefficient(n, k):
+    """
+    Logarithm of the binomial coefficient using Stirling's approximation
+    """
+    return (log_stirling_approximation(n) -
+            log_stirling_approximation(k) -
+            log_stirling_approximation(n - k))
+
+def log_prob_bin(k, n, r):
+    """
+    Logarithm of P(x = k), if X ~ Bin(n, r)
+    """
+    return log_binomial_coefficient(n, k) + k * torch.log(r + 1e-10) + (n - k) * torch.log(1 - r + 1e-10)
 
 def get_n_classes(dataset):
     if dataset in ["MUSH", "SVMGUIDE", "HABER", "TTT", "CODRNA", "ADULT", "PHIS"]: