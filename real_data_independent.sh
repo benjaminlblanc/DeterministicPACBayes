efb2582c8695f02e1c17cfce2cec4a2f582ffd27
beleb91 <beni-leblanc@hotmail.com>
2025-07-18 23:41:13 -0400

    [PATCH] Removed the experiments on the Renyi divergence.

--- real_data_independent.sh
+++ real_data_independent.sh
@@ -29,15 +29,4 @@ do
 
   r=Cbound
   python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.1 training.distribution=categorical model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=ones training.seed=$seed
-
-  r=Dis_Renyi
-  for ordr in 1.05 1.5 2 5 10
-  do
-    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.1 training.distribution=categorical model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=ones bound.order=$ordr bound.n_grid=5 training.seed=$seed
-  done
-  for ordr in 1.05 1.2 1.5 1.8 1.95
-  do
-    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.01 training.distribution=gaussian model.prior=0 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=ones bound.order=$ordr bound.n_grid=5 training.seed=$seed
-    python3 real.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.01 training.distribution=gaussian model.prior=0 training.risk=$r model.pred=$prd is_using_wandb=$wandb model.stump_init=rand bound.order=$ordr bound.n_grid=5 training.seed=$seed
-  done
 done
