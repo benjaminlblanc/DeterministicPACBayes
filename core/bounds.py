Corrected the computation for the DNN model.

core/bounds.py
@@ -50,7 +50,9 @@ def seeger_bound(n, model, risk, delta, div, sample=False, coeff=1, order=None, 
         else:
             const = np.log(2 * (n**0.5) / delta)
 
-    if sample:
+    if coeff == 0:
+        bound = risk
+    elif sample:
         bound = coeff * klInvFunction.apply(risk, torch.max(kl + const, torch.tensor(0)) / n)
     else:
         bound = coeff * klInvFunction.apply(risk, (kl + const) / n)

core/distributions.py
@@ -175,9 +175,12 @@ class Gaussian():
 
     def risk(self, batch, mean=True, centered=None):
         y_target, y_pred = batch
-        y_pred_oh = value_to_one_hot(y_pred, self.n_classes)
-        weighted_preds = y_pred_oh.transpose(1, 2) * self.w
-        summed_preds = torch.sum(weighted_preds, dim=2)
+        if len(self.w.shape) == 2:
+            summed_preds = torch.matmul(y_pred, self.w)
+        else:
+            y_pred_oh = value_to_one_hot(y_pred, self.n_classes)
+            weighted_preds = y_pred_oh.transpose(1, 2) * self.w
+            summed_preds = torch.sum(weighted_preds, dim=2)
         agg_preds = torch.argmax(summed_preds, dim=1).reshape(-1, 1)
 
         r = torch.where(y_target != agg_preds, torch.ones(1), torch.zeros(1))
@@ -206,12 +209,12 @@ class Gaussian():
         return sum(r)
 
     def rsample(self):
-
-        return Gaus(self.w, torch.eye(len(self.w))).rsample()
+        w = torch.reshape(self.w, (-1, 1))
+        return Gaus(w.squeeze(), torch.eye(len(w), dtype=torch.double)).rsample()
 
 
 distr_dict = {
+    "categorical": Categorical,
     "dirichlet": Dirichlet,
-    "gaussian": Gaussian,
-    "categorical": Categorical
+    "gaussian": Gaussian
 }

core/kl_inv.py
@@ -58,7 +58,7 @@ class klInvFunction(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_output):
         q, epsilon = ctx.saved_tensors
-
+        q = q.to(torch.float32)
         if torch.isclose(q, torch.tensor(0.)) or torch.isclose(q, torch.tensor(1.)) or ctx.out == 0. or ctx.out == 1.:
             return grad_output * 0., grad_output * 0., None
 

core/losses.py
@@ -1,14 +1,25 @@
 import torch
-from core.utils import BetaInc, Phi, mv_gaussian_cdf_precomputations, log_prob_bin
+from core.utils import BetaInc, Phi, gaussian_cdf_precomputations, log_prob_bin, value_to_one_hot
 
 
-def true_loss(y_target, y_pred, theta, distribution):
-    input = torch.where(y_target != y_pred, theta, -theta).sum(1)
-    heaviside = torch.heaviside(input, torch.tensor(0.))
-    if distribution == 'categorical':
-        return heaviside.detach() + input - input.detach()
-    elif distribution in ['dirichlet', 'gaussian']:
-        return [torch.sigmoid(inp) for inp in input]
+def true_loss(y_target, y_pred, theta, distribution, n_classes):
+    if distribution == "categorical":
+        y_pred_oh = value_to_one_hot(y_pred, n_classes)
+        weighted_preds = y_pred_oh.transpose(1, 2) * theta
+        summed_preds = torch.sum(weighted_preds, dim=2)
+        return torch.nn.CrossEntropyLoss()(summed_preds, y_target)
+    elif distribution == "dirichlet":
+        pass
+    elif distribution == "gaussian":
+        if len(theta.shape) == 1:
+            y_pred_oh = value_to_one_hot(y_pred, n_classes)
+            weighted_preds = y_pred_oh.transpose(1, 2) * theta
+            summed_preds = torch.sum(weighted_preds, dim=2)
+
+        else:
+            summed_preds = torch.matmul(y_pred, theta)
+        return torch.nn.CrossEntropyLoss(reduction='none')(summed_preds, y_target.squeeze())
+
 
 def triple_loss(y_target, y_pred, theta, predictor, distribution, n_classes):
     first_loss = moment_loss(y_target, y_pred, theta, predictor, distribution, n_classes, order=1)
@@ -43,9 +54,9 @@ def moment_loss(y_target, y_pred, theta, predictor, distribution, n_classes, ord
                 inner_Phi = (torch.squeeze(y_target) * torch.sum(torch.reshape(theta, (1, -1)) * y_pred, dim=1)) / torch.sum(y_pred ** 2, dim=1) ** 0.5
                 return Phi(inner_Phi) ** order
             else:
-                return mv_gaussian_cdf_precomputations(y_pred, y_target, theta, n_classes, torch.tensor(order))
+                return gaussian_cdf_precomputations(y_pred, y_target, theta, n_classes, torch.tensor(order), predictor)
 
         elif distribution == 'categorical':
             return torch.where(y_target != y_pred, theta, torch.tensor(0.)).sum(1) ** order
     elif predictor in ["resnet18"] and distribution == 'gaussian':
-        return None
+        return gaussian_cdf_precomputations(y_pred, y_target, theta, n_classes, torch.tensor(order), predictor)

core/utils.py
@@ -158,24 +158,23 @@ def value_to_one_hot(values, n_classes):
             array[i, j, int(values[i, j])] = 1
     return torch.tensor(array, dtype=torch.float)
 
-def create_mu(theta, y_pred, i):
+def create_mv_mu(theta, oh_y_pred_minus_oh_y_i):
     w = theta.reshape(1, len(theta))
-    y_pred_minus_y_i = y_pred - y_pred[:, :, [i]]
-    mu_full = torch.matmul(w, torch.transpose(y_pred_minus_y_i, 0, 2)).squeeze().T
-    correct_indexes = torch.arange(len(mu_full[0]))!=i
-    return mu_full[:, correct_indexes], y_pred_minus_y_i[:, :, correct_indexes]
+    mu_full = torch.matmul(w, torch.transpose(oh_y_pred_minus_oh_y_i, 0, 2)).squeeze().T
+    return mu_full
 
-def create_Sigma(y_pred, i):
-    y_pred_minus_y_i = y_pred - y_pred[:, :, [i]]
-    Sigma_full = torch.matmul(torch.transpose(y_pred_minus_y_i, 1, 2), y_pred_minus_y_i)
-    correct_indexes = torch.arange(len(Sigma_full[0])) != i
-    return Sigma_full[:, correct_indexes][:, :, correct_indexes]
+def create_mv_Sigma(oh_y_pred_minus_oh_y_i):
+    Sigma_full = torch.matmul(torch.transpose(oh_y_pred_minus_oh_y_i, 1, 2), oh_y_pred_minus_oh_y_i)
+    return Sigma_full
 
-def custom_truncated_mean_multi_normal(x, mu, S):
-    predictions = torch.distributions.multivariate_normal.MultivariateNormal(mu, S).sample(torch.Size([10000]))
-    is_inf = torch.prod(predictions <= x, dim=1)
-    predictions_lower = predictions[is_inf]
-    return torch.mean(predictions_lower, dtype=torch.float, dim=0)
+def create_nn_mu(theta, y_pred, i):
+    theta_minus_theta_i = theta - theta[:, [i]]
+    mu_full = torch.matmul(y_pred, theta_minus_theta_i)
+    correct_indexes = torch.arange(len(mu_full[0])) != i
+    return mu_full[:, correct_indexes]
+
+def create_nn_Sigma(y_pred):
+    return torch.sum(y_pred ** 2, dim=1)
 
 def create_notable_idx(unique_idx):
     len_unique_idx = len(unique_idx)
@@ -190,7 +189,7 @@ def create_notable_idx(unique_idx):
             elif j == len_unique_idx:
                 return notable_idx
 
-def purge_redundant_variables(y_pred_minus_y_i, mu, Sigma):
+def purge_redundant_mv_variables(y_pred_minus_y_i, mu, Sigma):
     y_preds_minus_y_i, mus, Sigmas = [], [], []
     for j in range(Sigma.shape[0]):
         diagonal_Sigma = torch.diag(Sigma[j])
@@ -210,29 +209,62 @@ def purge_redundant_variables(y_pred_minus_y_i, mu, Sigma):
 def is_psd(mat):
     return bool((mat == mat.T).all() and (torch.linalg.eigvals(mat).real>=0).all())
 
-def mv_gaussian_cdf_precomputations(y_pred, y_target, theta, n_classes, order):
+def gaussian_cdf_precomputations(y_pred, y_target, theta, n_classes, order, pred_type):
     cdfs = []
+    mus = []
+    Sigmas = []
     for i in range(n_classes):
         y_target_is_i = (y_target == i).squeeze()
         if torch.sum(y_target_is_i) > 0:
-            one_hot_y_pred = value_to_one_hot(y_pred[y_target_is_i], n_classes)
-            mu, y_pred_minus_y_i = create_mu(theta, one_hot_y_pred, i)
-            Sigma = create_Sigma(one_hot_y_pred, i)
-            purged_y_pred_minus_y_i, purged_mu, purged_Sigma = purge_redundant_variables(y_pred_minus_y_i, mu, Sigma)
-            for j in range(len(mu)):
-                cdfs.append(1 - MultinormalCDF.apply(purged_y_pred_minus_y_i[j], purged_mu[j], purged_Sigma[j]) ** order.item())
+            if pred_type == "rf":
+                one_hot_y_pred = value_to_one_hot(y_pred[y_target_is_i], n_classes)
+                oh_y_pred_minus_oh_y_i = one_hot_y_pred - one_hot_y_pred[:, :, [i]]
+                mu = create_mv_mu(theta, oh_y_pred_minus_oh_y_i)
+                Sigma = create_mv_Sigma(oh_y_pred_minus_oh_y_i)
+                purged_y_pred_minus_y_i, purged_mu, purged_Sigma = purge_redundant_mv_variables(oh_y_pred_minus_oh_y_i, mu, Sigma)
+                for j in range(len(mu)):
+                    cdfs.append(1 - MultinormalCDF.apply(purged_mu[j], purged_Sigma[j]) ** order.item())
+            else:
+                mus.append(create_nn_mu(theta, y_pred[y_target_is_i], i))
+                Sigmas.append(create_nn_Sigma(y_pred[y_target_is_i]))
+    mu = torch.vstack(mus)
+    Sigma = torch.hstack(Sigmas)
+    cdfs += 1 - NormalCDF.apply(mu, Sigma) ** order.item()
     return cdfs
 
 class MultinormalCDF(torch.autograd.Function):
     """Cumulative distribution function of the multivariate normal distribution; its forward and backward passes"""
 
     @staticmethod
-    def forward(ctx, one_hot_y_pred, purged_mu, purged_Sigma):
-        ctx.save_for_backward(one_hot_y_pred, purged_mu, purged_Sigma)
+    def forward(ctx, purged_mu, purged_Sigma):
+        ctx.save_for_backward(purged_mu, purged_Sigma)
         return torch.tensor(multivariate_normal.cdf(torch.zeros(len(purged_mu)), purged_mu, purged_Sigma, abseps=1e-2, releps=1e-2), dtype=torch.float32)
 
     @staticmethod
     def backward(ctx, grad):
-        purged_y_pred_minus_y_i, purged_mu, purged_Sigma = ctx.saved_tensors
-        expected_neg = -torch.sqrt(2 / math.pi * torch.diagonal(purged_Sigma))
-        return torch.tensor(0), grad * torch.matmul(torch.inverse(purged_Sigma), expected_neg), torch.tensor(0)
+        purged_mu, purged_Sigma = ctx.saved_tensors
+        truncated_expectation = -torch.sqrt(torch.diagonal(purged_Sigma) / (2 * math.pi)) * torch.exp(-1/2 * purged_mu ** 2 / purged_Sigma)
+        return grad * torch.matmul(torch.inverse(purged_Sigma), truncated_expectation), torch.tensor(0)
+
+class NormalCDF(torch.autograd.Function):
+    """Cumulative distribution function of the normal distribution; its forward and backward passes"""
+
+    @staticmethod
+    def forward(ctx, mu, Sigma):
+        ctx.save_for_backward(mu, Sigma)
+        return torch.prod(1/2 * (1 - erf_approximation(mu / (torch.sqrt(2 * (2 * Sigma.unsqueeze(1).repeat(1, mu.shape[1])))))), dim=1).to(torch.double)
+
+    @staticmethod
+    def backward(ctx, grad):
+        mu, Sigma = ctx.saved_tensors
+        Sigma_repeated = Sigma.unsqueeze(1).repeat(1, mu.shape[1])
+        truncated_expectation = -torch.sqrt((2 * Sigma_repeated) / (2 * math.pi)) * torch.exp(-1/2 * mu ** 2 / (2 * Sigma_repeated))
+        return grad.unsqueeze(1).repeat(1, mu.shape[1]) * (2 * Sigma_repeated) ** -1 * truncated_expectation, torch.tensor(0)
+
+
+def erf_approximation(x):
+    x = torch.clamp(x, -2, 2)
+    summed = 0
+    for i in range(20):
+        summed += ((-1) ** i * x ** (2 * i + 1)) / (math.factorial(i) * (2 * i + 1))
+    return summed * 2 / math.sqrt(math.pi)

models/pretrainedDNN.py
@@ -4,14 +4,14 @@ from core.distributions import Gaussian
 
 
 def pretrainedDNN(pred):
-    if pred == 'resnet18':
-        model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', weights="ResNet18_Weights.DEFAULT")
+    if pred[:6] == 'resnet':
+        model = torch.hub.load('pytorch/vision:v0.10.0', pred, weights=f"ResNet{pred[6:]}_Weights.DEFAULT")
 
         # We only keep the embedding
         model.fc = nn.Identity()
         model.eval()
     else:
-        raise NotImplementedError("model.pred should be one the following: [stumps-uniform, rf, resnet152]")
+        raise NotImplementedError("model.pred should be one the following: stumps-uniform, rf, resnet18, resnet34, resnet50, resnet101, resnet152.")
     return model
 
 class LinearMultiClassifier(torch.nn.Linear):
@@ -20,8 +20,7 @@ class LinearMultiClassifier(torch.nn.Linear):
         if distr != "gaussian":
             raise NotImplementedError
 
-        self.num_voters = len(prior)
-        post = torch.rand(self.num_voters) * 4 - 2  # uniform draws in [-2, 2]
+        post = torch.normal(0, 0.01, prior.shape, dtype=dtype)
         self.post = torch.nn.Parameter(post, requires_grad=True)
 
         self.prior = prior
@@ -33,12 +32,12 @@ class LinearMultiClassifier(torch.nn.Linear):
     def forward(self, x):
         return x
 
-    def risk(self, batch, loss=None, mean=True):
+    def risk(self, batch, loss=None, mean=True, centered=True):
 
         if loss is not None:
             return self.distribution.approximated_risk(batch, loss, mean)
 
-        return self.distribution.risk(batch, mean)
+        return self.distribution.risk(batch, mean, centered=centered)
 
     def KL(self):
 
@@ -60,24 +59,9 @@ class LinearMultiClassifier(torch.nn.Linear):
         return self.post.grad
 
     def set_post(self, value):
-
-        assert len(value) == self.num_voters
-
-        if self.distr_name == "categorical": # make sure params sum to 1
-            self.post = torch.nn.Parameter(value, requires_grad=True)
-            self.distribution.theta = self.post
-
-        elif self.distribution_name == "dirichlet":
-            assert all(value > 0), "all posterior parameters must be positive"
-            self.post = torch.nn.Parameter(torch.log(value), requires_grad=True) # use log (and apply exp(post) later so that posterior parameters are always positive)
-            self.distribution.alpha = self.post
-
-        elif self.distribution_name == "gaussian":
-            self.post = torch.nn.Parameter(value, requires_grad=True)
-            self.distribution.w = self.post
+        self.post = torch.nn.Parameter(value, requires_grad=True)
+        self.distribution.w = self.post
 
     def random_draw_new_post(self):
         value = self.distribution.rsample()
-        if self.distribution_name == "dirichlet":
-            value *= self.prior.sum()
-        self.set_post(value)
+        self.set_post(value.reshape(-1, self.n_classes))