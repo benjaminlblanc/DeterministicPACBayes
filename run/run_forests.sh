90fb0f48554aa17246b49d27ac1d58b6346a75c9
beleb91 <beni-leblanc@hotmail.com>
2025-08-29 08:56:40 -0400

    Changed a dataset to train on.

--- run/run_forests.sh
+++ run/run_forests.sh
@@ -5,7 +5,7 @@ m=100
 trials=5
 seed=541944
 
-for d in FASHION MNIST PENDIGITS PROTEIN SHUTTLE
+for d in FASHION MNIST PENDIGITS PROTEIN SENSORLESS
 do
   r=FO
   python3 training.py project_name=$name num_trials=$trials dataset=$d model.M=$m training.lr=0.1 training.distribution=categorical model.prior=adjusted training.risk=$r model.pred=$prd is_using_wandb=$wandb training.seed=$seed model.output=class model.max_tree_depth=None
